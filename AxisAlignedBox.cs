ad23b5a9c939fb33224b05e8b0209d6287a9a15a
Milk8 <77039660+Milk8@users.noreply.github.com>
2021-04-20 23:01:55 -0500

    Add files via upload

---
+++ AxisAlignedBox.cs
@@ -0,0 +1,7 @@
+﻿
+namespace AssaultCubeHack {
+    public struct AxisAlignedBox {
+        public Vector4 min;
+        public Vector4 max;
+    }
+}
---
+++ Vector2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssaultCubeHack {
+    class Vector2 {
+
+        public float x;
+        public float y;
+
+        public Vector2() {}
+
+        public Vector2(float x, float y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public float Distance(Vector2 vector) {
+            float dx = vector.x - x;
+            float dy = vector.y - y;
+            return (float)Math.Sqrt(dx * dx + dy * dy);
+        }
+
+        public override string ToString() {
+            return string.Format("{0}, {1}", Math.Round(x, 2), Math.Round(y, 2));
+        }
+    }
+}
